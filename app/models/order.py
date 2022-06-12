7876f9cf7c7eaec69e89b37984c37a39a6f216f7
Conrado Carrizo <conradocarrizo@gmail.com>
2022-06-12 14:46:39 -0300

    models

--- app/models/order.py
+++ app/models/order.py
@@ -1,4 +1,3 @@
-import stat
 from django.db import models
 
 
@@ -6,8 +5,11 @@ class Order(models.Model):
 
     date_time = models.DateTimeField()
 
-    def get_total(self):
-        pass
+    def get_total(self) -> float:
+        return sum(details.sub_total for details in self.details.all())
 
     def get_total_usd(self):
         pass
+
+    def __str__(self):
+        return f"ID: {self.id} total: ${self.get_total()}"
