d1a483a6fb2d9fc73de89e32e21ffaf5dd89c259
Conrado Carrizo <conradocarrizo@gmail.com>
2022-06-12 14:48:14 -0300

    adding singals to appconfig

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from django.db.models.signals import pre_save
 
 
-class AppConfig(AppConfig):
+class EcommerceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+
+    def ready(self):
+        from app import signals
