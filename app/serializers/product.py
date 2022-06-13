003e5c0b0c427d3641d52a55f4a8765724fae88e
Conrado Carrizo <conradocarrizo@gmail.com>
2022-06-13 10:00:05 -0300

    update product serializer

--- app/serializers/product.py
+++ app/serializers/product.py
@@ -4,6 +4,9 @@ from app.models.product import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    id = serializers.CharField()
+    name = serializers.CharField(required=False)
+    price = serializers.FloatField(required=False)
 
     class Meta:
         model = Product
