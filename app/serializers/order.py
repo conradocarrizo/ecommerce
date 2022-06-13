67e860666cdf2068231fe488e87534e0e94669e8
Conrado Carrizo <conradocarrizo@gmail.com>
2022-06-13 09:59:26 -0300

    order and order_detail serializers

--- app/serializers/order.py
+++ app/serializers/order.py
@@ -1,18 +1,71 @@
+from typing import List
 from rest_framework import serializers
+from app.errors import DuplicateProductDetailOrdenError
 from app.models.order import Order
+from app.models.order_detail import OrderDetail
+from app.serializers.order_detail import OrderDetailSerializer
+from django.db import IntegrityError, transaction
 
 
 class OrderSerializer(serializers.ModelSerializer):
     total = serializers.SerializerMethodField()
     total_usd = serializers.SerializerMethodField()
+    details = OrderDetailSerializer(many=True, required=False)
 
     class Meta:
         model = Order
-        fields = ['date_time', 'total', 'total_usd', 'details']
-        depth = 2
+        fields = ['id', 'date_time', 'total', 'total_usd', 'details']
+        depth = 3
 
     def get_total(self, obj: Order):
         return obj.get_total()
 
     def get_total_usd(self, obj: Order):
         return obj.get_total_usd()
+
+    def create(self, validated_data):
+        instance = Order.objects.create(date_time=validated_data.get("date_time"))
+        for detail in validated_data["details"]:
+            try:
+                OrderDetail.objects.create(
+                    order=instance,
+                    product_id=detail.get("product").get("id"),
+                    quantity=detail.get("quantity")
+                )
+            except IntegrityError as error:
+                if "UNIQUE constraint failed" in str(error):
+                    raise DuplicateProductDetailOrdenError(
+                        product_name=detail.get("product").get("name"))
+                else:
+                    raise error
+        instance.refresh_from_db()
+        return instance
+
+    def update(self, instance, validated_data):
+        with transaction.atomic():
+            date_time = validated_data.get("date_time")
+            if instance.date_time != date_time:
+                instance.date_time = date_time
+                instance.save()
+
+            for detail in validated_data["details"]:
+                if detail.get("pk"):
+                    order_detail = OrderDetail.objects.get(id=detail["pk"])
+                    if order_detail and order_detail.quantity != detail.get("quantity"):
+                        order_detail.quantity = detail.get("quantity")
+                        order_detail.save()
+                else:
+                    try:
+                        OrderDetail.objects.create(
+                            order=instance,
+                            product_id=detail.get("product").get("id"),
+                            quantity=detail.get("quantity")
+                        )
+                    except IntegrityError as error:
+                        if "UNIQUE constraint failed" in str(error):
+                            raise DuplicateProductDetailOrdenError(
+                                product_name=detail.get("product").get("name"))
+                        else:
+                            raise error
+        instance.refresh_from_db()
+        return instance
---
+++ app/serializers/order_detail.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from app.models.order_detail import OrderDetail
+from app.serializers.product import ProductSerializer
+
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    sub_total = serializers.ReadOnlyField()
+    product = ProductSerializer()
+    pk = serializers.IntegerField(required=False)
+
+    class Meta:
+        model = OrderDetail
+        fields = ['pk',"quantity", "sub_total",'product']
+        depth = 3
+
+    def get_sub_total(self, obj: OrderDetail):
+        return obj.sub_total
