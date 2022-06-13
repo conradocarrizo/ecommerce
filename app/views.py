22437f279ec568b169e3a0874556d6a4d8c01c14
Conrado Carrizo <conradocarrizo@gmail.com>
2022-06-12 21:15:49 -0300

    Product ModelViewSet view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from app.models.order import Order
+from app.models.product import Product
+from app.serializers.order import OrderSerializer
 
-# Create your views here.
+from app.serializers.product import ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+    def destroy(self, request, *args, **kwargs):
+        product = self.get_object()
+        product.delete()
+        return Response({"message": "Product has been deleted"}, status=204)
