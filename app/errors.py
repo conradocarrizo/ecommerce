fc99c8af0a3b3d8edcacbf345a5c22bffcc627a4
Conrado Carrizo <conradocarrizo@gmail.com>
2022-06-13 09:55:57 -0300

    update errors

--- app/errors.py
+++ app/errors.py
@@ -1,11 +1,31 @@
-from rest_framework.exceptions import APIException
-
-class InvalidStockProduct(Exception):
+class InvalidProductStock(Exception):
     message: str = "the stock must be a positive number"
 
     def __init__(self, stock=None):
         if stock:
             self.message = self.message + f", {stock} is not"
+
+    def __str__(self):
+        return self.message
+
+
+class NotEnoughtStockError(Exception):
+    message: str = "not enought stock"
+
+    def __init__(self, product_name=None):
+        if product_name:
+            self.message = self.message + f" of {product_name}"
+
+    def __str__(self):
+        return self.message
+
+
+class DuplicateProductDetailOrdenError(Exception):
+    message: str = "There can only be one detail order for each product in the order"
+
+    def __init__(self, product_name=None):
+        if product_name:
+            self.message = self.message + f"{product_name} more than once"
 
     def __str__(self):
         return self.message
--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,9 @@
 from requests import Response
 from rest_framework.views import exception_handler
 
-from app.errors import InvalidStockProduct
+from app.errors import InvalidProductStock, NotEnoughtStockError, DuplicateProductDetailOrdenError
 from rest_framework.response import Response
+
 
 def custom_exception_handler(exc, context):
 
@@ -10,5 +11,11 @@ def custom_exception_handler(exc, context):
     if response is not None:
         return response
 
-    if isinstance(exc, InvalidStockProduct):
+    if isinstance(exc, DuplicateProductDetailOrdenError):
+        return Response({"error": exc.message}, status=400)
+
+    if isinstance(exc, InvalidProductStock):
+        return Response({"error": exc.message}, status=400)
+
+    if isinstance(exc, NotEnoughtStockError):
         return Response({"error": exc.message}, status=400)
